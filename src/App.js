27c5dae72baba796c81a6fae2a5c23eea323a5f9
Kireev Igor <fury777333@gmail.com>
2018-03-22 16:19:45 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -6,18 +6,15 @@ import Youtube from './img/youtube.svg';
 import Twitter from './img/twitter.svg';
 import Google from './img/google-plus.svg';
 import RightMenu from './RightMenu';
+import Bottom from './Bottom';
+import Header from './Vivod';
 
 class App extends Component {
  
-constructor(props){
-  super(props);
-  this.state={
+  state={
     date: [], 
-    name: '', 
     loading: true
-  };
-}
-
+  }
   getQuery = url => {
     return new Promise((succeed, fail)=>{
       const request = new XMLHttpRequest();
@@ -49,8 +46,22 @@ constructor(props){
             items => {
              this.setState({data: items});
             })
+
+
+
+
+    /*const request = new XMLHttpRequest();
+request.open("GET", "https://api.hh.ru/vacancies, false);
+request.send();
+const status = request.status;
+if(status==200)
+document.write("Текст ответа: " + request.responseText)
+else if(status==404)
+document.write("Ресурс не найден")
+else
+document.write(request.statusText)
+*/
   }
-  
   handleChange = event => {
     this.setState({
       name: event.target.value,
@@ -79,46 +90,27 @@ constructor(props){
   
  
 
-  render(){
-    const {data} = this.state;
+  render() {
+    const {data} = this.props.data;
     return (
+      /*loading ? <div>Загрузка</div> :*/ 
+
+      
       <div>
-         <RightMenu />
+<RightMenu/>
+
 
         <div className="menu">
         <form className="form_">
-         <input placeholder="Ввод информации:" className="pole" type="search" value={this.state.name}
-          onChange={this.handleChange} />
-        <button className="knopka" onClick={this.handleClickSearch}>
-        Поиск
-    </button>
-        <button className="reg">
+         <input placeholder="Ввод информации:" class="pole" type="search" value={this.state.name} onChange={this.handleChange}  />
+        <button class="knopka" onClick={this.handleClickSearch}>
         Поиск
     </button>
     </form>
     </div>
     <div className="viravnivanie"></div>
-        {console.log(data)}
-        {data && data.map((data,index) => {
-          return (  
-          <div key={index}>
-          
-          <div className="vivod">
-            <div><tr><th><h2><a href={data.alternate_url}>{data.name}</a></h2></th></tr></div>
-            <div><hr></hr><tr><td><p>Требования:{data.snippet.requirement}</p></td></tr></div>
-            <div><tr><td><p>Город:{data.area.name}</p></td></tr></div>
-            <div><tr><td><p>Статус:{data.type.name}</p></td></tr></div>
-                </div>
-            </div>
-            
-  
-          )
-        })}
-         <div className="bottom"><div><img src={Instagram}
-          className="img_soc"/><img src={Youtube} 
-          className="img_soc_2"/><img src={Twitter} className="img_soc_4"/><img src={Google} 
-          className="img_soc_3"/><p>Благодарим за посищение сайта(С)</p></div>
-          </div>
+
+        <Bottom/>
       </div>
     );
   }
